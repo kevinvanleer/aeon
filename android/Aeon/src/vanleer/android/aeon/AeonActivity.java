c0334d5af5f353550e9bb7509ee90dcbc53513c2
kvanleer <kevin.vanleer@gmail.com>
2011-08-28 08:21:20 -0500

    Added AeonActivity.java to source control, don't know how this one got missed previously.

--- android/Aeon/src/vanleer/android/aeon/AeonActivity.java
+++ android/Aeon/src/vanleer/android/aeon/AeonActivity.java
@@ -2,12 +2,24 @@ package vanleer.android.aeon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AeonActivity extends Activity {
+	final Itinerary aeonItinerary = new Itinerary(this);
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);        
+        
+        final Button planButton = (Button) findViewById(R.id.button_plan);
+        planButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {            	
+            	setContentView(R.layout.itinerary);
+            }
+            
+                // Perform action on click
+        });    
     }
 }
