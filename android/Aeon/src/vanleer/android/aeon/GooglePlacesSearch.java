252a03b2d06a5aa42f45724c570d42d624bda34a
kvanleer <kevin.vanleer@gmail.com>
2012-03-24 22:03:23 -0500

    (26964199) Updated search to match query to place types.

--- android/Aeon/src/vanleer/android/aeon/GooglePlacesSearch.java
+++ android/Aeon/src/vanleer/android/aeon/GooglePlacesSearch.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.lang.Math;
 
 import org.apache.http.HttpResponse;
@@ -28,22 +29,55 @@ public final class GooglePlacesSearch {
 	private static final String GOOGLE_GEOCODING_URL = "https://maps.googleapis.com/maps/api/geocode/json";
 	private static final String GOOGLE_DISTANCE_MATRIX_URL = "https://maps.googleapis.com/maps/api/distancematrix/json";
 	private String apiKey = null;
-	private HttpClient httpClient = null;
+	private HttpClient httpClient = new DefaultHttpClient();
 	private boolean autocomplete = false;
 	private JSONObject placesSearchResults = null;
 	private JSONObject geocodingSearchResults = null;
-	private ArrayList<ItineraryItem> places = null;
+	private ArrayList<ItineraryItem> places = new ArrayList<ItineraryItem>();
 	private ItineraryItemDistanceComparator distanceCompare = new ItineraryItemDistanceComparator();
+	private ArrayList<String> placeTypes = new ArrayList<String>();
 
 	GooglePlacesSearch(String userApiKey, String userClientId) {
 		apiKey = userApiKey;
-		places = new ArrayList<ItineraryItem>();
-		httpClient = new DefaultHttpClient();
+		InitializePlaceTypes();
 	}
 
 	void PerformSearch(double latitude, double longitude,
 			double radius, String name, boolean sensor) {
-		PerformSearch(latitude, longitude, radius, null, name, sensor);
+		String type = FindType(name);
+		if(type != null)
+		{
+			String[] types = new String[1];
+			types[0] = type;
+			PerformSearch(latitude, longitude, radius, types, "", sensor);
+		}
+		else
+		{
+			PerformSearch(latitude, longitude, radius, null, name, sensor);
+		}
+	}
+
+	private String FindType(String name) {
+		String inferredType = null;
+		
+		name = name.toLowerCase();
+		if(name.endsWith("es")) {
+			name = name.substring(0, (name.length() - 2));
+		}
+		if(name.endsWith("s")) {
+			name = name.substring(0, (name.length() - 1));
+		}
+		
+		Iterator<String> itr = placeTypes.iterator();
+		while(itr.hasNext()) {
+			String type = itr.next();
+			if(name.compareTo(type.replace("_", " ")) == 0) {
+				inferredType = type; 
+				break;
+			}
+		}
+		
+		return inferredType;
 	}
 
 	void PerformSearch(double latitude, double longitude,
@@ -279,5 +313,107 @@ public final class GooglePlacesSearch {
 		String lngString = String.format("%1$.4f", Math.abs(location.getLongitude()));
 
 		return (latString + latSuffix + ", " + lngString + lngSuffix);
+	}
+	
+	private void InitializePlaceTypes() {
+		//Levenshtein distance
+		//frej
+		placeTypes.add("accounting");
+		placeTypes.add("airport");
+		placeTypes.add("amusement_park");
+		placeTypes.add("aquarium");
+		placeTypes.add("art_gallery");
+		placeTypes.add("atm");
+		placeTypes.add("bakery");
+		placeTypes.add("bank");
+		placeTypes.add("bar");
+		placeTypes.add("beauty_salon");
+		placeTypes.add("bicycle_store");
+		placeTypes.add("book_store");
+		placeTypes.add("bowling_alley");
+		placeTypes.add("bus_station");
+		placeTypes.add("cafe");
+		placeTypes.add("campground");
+		placeTypes.add("car_dealer");
+		placeTypes.add("car_rental");
+		placeTypes.add("car_repair");
+		placeTypes.add("car_wash");
+		placeTypes.add("casino");
+		placeTypes.add("cemetery");
+		placeTypes.add("church");
+		placeTypes.add("city_hall");
+		placeTypes.add("clothing_store");
+		placeTypes.add("convenience_store");
+		placeTypes.add("courthouse");
+		placeTypes.add("dentist");
+		placeTypes.add("department_store");
+		placeTypes.add("doctor");
+		placeTypes.add("electrician");
+		placeTypes.add("electronics_store");
+		placeTypes.add("embassy");
+		placeTypes.add("establishment");
+		placeTypes.add("finance");
+		placeTypes.add("fire_station");
+		placeTypes.add("florist");
+		placeTypes.add("food");
+		placeTypes.add("funeral_home");
+		placeTypes.add("furniture_store");
+		placeTypes.add("gas_station");
+		placeTypes.add("general_contractor");
+		placeTypes.add("geocode");
+		placeTypes.add("grocery_or_supermarket");
+		placeTypes.add("gym");
+		placeTypes.add("hair_care");
+		placeTypes.add("hardware_store");
+		placeTypes.add("health");
+		placeTypes.add("hindu_temple");
+		placeTypes.add("home_goods_store");
+		placeTypes.add("hospital");
+		placeTypes.add("insurance_agency");
+		placeTypes.add("jewelry_store");
+		placeTypes.add("laundry");
+		placeTypes.add("lawyer");
+		placeTypes.add("library");
+		placeTypes.add("liquor_store");
+		placeTypes.add("local_government_office");
+		placeTypes.add("locksmith");
+		placeTypes.add("lodging");
+		placeTypes.add("meal_delivery");
+		placeTypes.add("meal_takeaway");
+		placeTypes.add("mosque");
+		placeTypes.add("movie_rental");
+		placeTypes.add("movie_theater");
+		placeTypes.add("moving_company");
+		placeTypes.add("museum");
+		placeTypes.add("night_club");
+		placeTypes.add("painter");
+		placeTypes.add("park");
+		placeTypes.add("parking");
+		placeTypes.add("pet_store");
+		placeTypes.add("pharmacy");
+		placeTypes.add("physiotherapist");
+		placeTypes.add("place_of_worship");
+		placeTypes.add("plumber");
+		placeTypes.add("police");
+		placeTypes.add("post_office");
+		placeTypes.add("real_estate_agency");
+		placeTypes.add("restaurant");
+		placeTypes.add("roofing_contractor");
+		placeTypes.add("rv_park");
+		placeTypes.add("school");
+		placeTypes.add("shoe_store");
+		placeTypes.add("shopping_mall");
+		placeTypes.add("spa");
+		placeTypes.add("stadium");
+		placeTypes.add("storage");
+		placeTypes.add("store");
+		placeTypes.add("subway_station");
+		placeTypes.add("synagogue");
+		placeTypes.add("taxi_stand");
+		placeTypes.add("train_station");
+		placeTypes.add("travel_agency");
+		placeTypes.add("university");
+		placeTypes.add("veterinary_care");
+		placeTypes.add("zoo");
 	}
 }
