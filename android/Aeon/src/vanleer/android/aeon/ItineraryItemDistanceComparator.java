33e0e8e2abe584224f7c53b77abb488b154e11b1
kvanleer <kevin.vanleer@gmail.com>
2012-03-27 22:33:13 -0500

    (27136847): Added check to compare method to send null distances to end of list

--- android/Aeon/src/vanleer/android/aeon/ItineraryItemDistanceComparator.java
+++ android/Aeon/src/vanleer/android/aeon/ItineraryItemDistanceComparator.java
@@ -5,11 +5,21 @@ import java.util.Comparator;
 public class ItineraryItemDistanceComparator implements Comparator<ItineraryItem> {
 	public int compare(ItineraryItem leftItem, ItineraryItem rightItem) {
 		int compared = 0;
-		if(leftItem.GetDistance() < rightItem.GetDistance()) {
-			compared = -1;
+		try {
+			if(leftItem.GetDistance() < rightItem.GetDistance()) {
+				compared = -1;
+			}
+			else if(leftItem.GetDistance() > rightItem.GetDistance()) {
+				compared = 1;
+			}
 		}
-		else if(leftItem.GetDistance() > rightItem.GetDistance()) {
-			compared = 1;
+		catch(NullPointerException e) {
+			if(leftItem.GetDistance() == null) {
+				compared = 1;
+			}
+			else if(rightItem.GetDistance() == null) {
+				compared = -1;
+			}
 		}
 		return compared;
 	}
