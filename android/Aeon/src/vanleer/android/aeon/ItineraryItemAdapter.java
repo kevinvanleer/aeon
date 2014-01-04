996e36c8702d1a3e130c08b9df2104b55fc29be6
kvanleer <kevin.vanleer@gmail.com>
2014-01-04 09:58:43 -0600

    Added default color list Applied auto formatting

--- android/Aeon/src/vanleer/android/aeon/ItineraryItemAdapter.java
+++ android/Aeon/src/vanleer/android/aeon/ItineraryItemAdapter.java
@@ -3,6 +3,8 @@ package vanleer.android.aeon;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +13,8 @@ import android.widget.TextView;
 
 class ItineraryItemAdapter extends ArrayAdapter<ItineraryItem> {
 
-	private ArrayList<ItineraryItem> destinationList;
+	private final ArrayList<ItineraryItem> destinationList;
+	private ColorStateList defaultColors = null;
 
 	public ItineraryItemAdapter(Context context, int textViewResourceId, ArrayList<ItineraryItem> items) {
 		super(context, textViewResourceId);
@@ -22,13 +25,12 @@ class ItineraryItemAdapter extends ArrayAdapter<ItineraryItem> {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View v = convertView;
 		if (v == null) {
-			LayoutInflater vi =
-					(LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			LayoutInflater vi = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			v = vi.inflate(R.layout.itinerary_item, null);
 		}
-		if(!destinationList.isEmpty()) {
+		if (!destinationList.isEmpty()) {
 			ItineraryItem item = destinationList.get(position);
-			
+
 			if (item != null) {
 				TextView destinationName = (TextView) v.findViewById(R.id.textView_destinationName);
 				TextView stayDuration = (TextView) v.findViewById(R.id.textView_stayDuration);
@@ -39,7 +41,11 @@ class ItineraryItemAdapter extends ArrayAdapter<ItineraryItem> {
 				TextView travelDistance = (TextView) v.findViewById(R.id.textView_travelDistance);
 				TextView travelTime = (TextView) v.findViewById(R.id.textView_travelTime);
 
-				//TODO: Get type of transportation used
+				if (defaultColors == null) {
+					defaultColors = destinationName.getTextColors();
+				}
+
+				// TODO: Get type of transportation used
 				travelDistance.setText("Drive/Walk/Bike/Ride " + item.getFormattedDistance());
 				travelTime.setText(" in " + item.getTravelDurationLongFormat());
 				arrivalVicinity.setText("Arrive at " + item.GetVicinity());
@@ -48,9 +54,19 @@ class ItineraryItemAdapter extends ArrayAdapter<ItineraryItem> {
 				stayDuration.setText(" for " + item.getSchedule().getStayDurationLongFormat());
 				departureVicinity.setText("Depart from " + item.GetVicinity());
 				departureTime.setText(" at " + item.getSchedule().getDepartureTimeString());
-				
-				if(item.getName() == "Press to add new destination") {
-					//HACK: Make this better
+
+				travelDistance.setVisibility(View.VISIBLE);
+				travelTime.setVisibility(View.VISIBLE);
+				arrivalVicinity.setVisibility(View.VISIBLE);
+				arrivalTime.setVisibility(View.VISIBLE);
+				stayDuration.setVisibility(View.VISIBLE);
+				departureVicinity.setVisibility(View.VISIBLE);
+				departureTime.setVisibility(View.VISIBLE);
+
+				destinationName.setTextColor(defaultColors);
+
+				if (item.getName() == "Press to add new destination") {
+					// HACK: Make this better
 					arrivalVicinity.setVisibility(View.GONE);
 					arrivalTime.setVisibility(View.GONE);
 					travelDistance.setVisibility(View.GONE);
@@ -58,38 +74,29 @@ class ItineraryItemAdapter extends ArrayAdapter<ItineraryItem> {
 					stayDuration.setVisibility(View.GONE);
 					departureVicinity.setVisibility(View.GONE);
 					departureTime.setVisibility(View.GONE);
-				} else if(position == 0) {
+
+					// destinationName.setTextColor(Color.BLACK);
+					// destinationName.setBackgroundColor(Color.WHITE);
+				} else if (position == 0) {
 					arrivalVicinity.setVisibility(View.GONE);
 					arrivalTime.setVisibility(View.GONE);
 					travelDistance.setVisibility(View.GONE);
 					travelTime.setVisibility(View.GONE);
 					stayDuration.setVisibility(View.GONE);
-					departureVicinity.setVisibility(View.VISIBLE);
-					departureTime.setVisibility(View.VISIBLE);
 
 					departureVicinity.setText("Start from " + item.GetVicinity());
-				} else if(position == (destinationList.size() - 1)) {
-					travelDistance.setVisibility(View.VISIBLE);
-					travelTime.setVisibility(View.VISIBLE);
-					arrivalVicinity.setVisibility(View.VISIBLE);
-					arrivalTime.setVisibility(View.VISIBLE);
+				} else if (position == (destinationList.size() - 1)) {
 					stayDuration.setVisibility(View.GONE);
 					departureVicinity.setVisibility(View.GONE);
 					departureTime.setVisibility(View.GONE);
-					
+
 					arrivalVicinity.setText("End at " + item.GetVicinity());
 				} else {
-					travelDistance.setVisibility(View.VISIBLE);
-					travelTime.setVisibility(View.VISIBLE);
-					arrivalVicinity.setVisibility(View.VISIBLE);
-					arrivalTime.setVisibility(View.VISIBLE);
-					stayDuration.setVisibility(View.VISIBLE);
-					departureVicinity.setVisibility(View.VISIBLE);
-					departureTime.setVisibility(View.VISIBLE);
-				}			
+
+				}
 			}
 		}
-		
+
 		return v;
 	}
 }
