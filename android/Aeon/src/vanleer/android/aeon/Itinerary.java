2ef944ed0784b31990208fa7b71b5043d13d4e70
kvanleer <kevin.vanleer@gmail.com>
2014-01-04 10:13:42 -0600

    Fixed typo in parameter name Applied auto formatting

--- android/Aeon/src/vanleer/android/aeon/Itinerary.java
+++ android/Aeon/src/vanleer/android/aeon/Itinerary.java
@@ -11,6 +11,7 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -27,16 +28,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemLongClickListener;
 
-//Something for sending destinations to Navigator
-//I/ActivityManager(  118): Starting activity: Intent { act=android.intent.action.VIEW dat=google.navigation:///?q=Some%20place cmp=brut.googlemaps/com.google.android.maps.driveabout.app.NavigationActivity }
+// Something for sending destinations to Navigator
+// I/ActivityManager( 118): Starting activity: Intent { act=android.intent.action.VIEW dat=google.navigation:///?q=Some%20place cmp=brut.googlemaps/com.google.android.maps.driveabout.app.NavigationActivity }
 
 public final class Itinerary extends Activity implements OnClickListener {
 
-	private int listViewId = R.id.listView_itinerary;
+	private final int listViewId = R.id.listView_itinerary;
 	private ListView itineraryListView;
 	private ArrayList<ItineraryItem> itineraryItemList;
 	private ItineraryItemAdapter itineraryItems;
-	private boolean loggedIntoGoogle = /* false */true; // for debugging
+	private final boolean loggedIntoGoogle = /* false */true; // for debugging
 	private LocationManager locationManager;
 	private Location currentLocation = null;
 	private static final int GET_NEW_DESTINATION = 0;
@@ -44,7 +45,7 @@ public final class Itinerary extends Activity implements OnClickListener {
 	private ProgressDialog waitSpinner;
 	private boolean waitingForGps = false;
 	private ItineraryItem origin = null;
-	private ItineraryItem andNewItemItem = null;
+	private ItineraryItem addNewItemItem = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -58,23 +59,19 @@ public final class Itinerary extends Activity implements OnClickListener {
 		// FOR TESTING
 
 		itineraryItemList = new ArrayList<ItineraryItem>();
-		itineraryItems = new ItineraryItemAdapter(this,
-				R.layout.itinerary_item, itineraryItemList);
+		itineraryItems = new ItineraryItemAdapter(this, R.layout.itinerary_item, itineraryItemList);
 		itineraryListView = (ListView) findViewById(listViewId);
 		itineraryListView.setAdapter(itineraryItems);
 
 		// Acquire a reference to the system Location Manager
-		locationManager = (LocationManager) this
-				.getSystemService(Context.LOCATION_SERVICE);
+		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 		LocationListener locationListener = new LocationListener() {
 			public void onLocationChanged(Location location) {
-				// Called when a new location is found by the network location
-				// provider.
+				// Called when a new location is found by the network location provider.
 				onNewLocation(location);
 			}
 
-			public void onStatusChanged(String provider, int status,
-					Bundle extras) {
+			public void onStatusChanged(String provider, int status, Bundle extras) {
 			}
 
 			public void onProviderEnabled(String provider) {
@@ -84,23 +81,20 @@ public final class Itinerary extends Activity implements OnClickListener {
 			}
 		};
 
-		// Register the listener with the Location Manager to receive location
-		// updates
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
-				0, locationListener);
+		// Register the listener with the Location Manager to receive location updates
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
 		configureItineraryListViewLongClickListener();
 		initializeOrigin();
 		initializeAddNewItineraryItem();
 	}
-	
+
 	private void initializeAddNewItineraryItem() {
-		andNewItemItem = new ItineraryItem("Press to add new destination");
-		LayoutInflater vi =
-				(LayoutInflater) this.getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		addNewItemItem = new ItineraryItem("Press to add new destination");
+		LayoutInflater vi = (LayoutInflater) this.getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View v = vi.inflate(R.layout.itinerary_item, null);
-		
-		//TextView destinationName = (TextView) v.findViewById(R.id.textView_destinationName);		
+
+		// TextView destinationName = (TextView) v.findViewById(R.id.textView_destinationName);
 		TextView arrivalVicinity = (TextView) v.findViewById(R.id.textView_arrivalLocation);
 		TextView arrivalTime = (TextView) v.findViewById(R.id.textView_arrivalTime);
 		TextView travelDistance = (TextView) v.findViewById(R.id.textView_travelDistance);
@@ -108,7 +102,7 @@ public final class Itinerary extends Activity implements OnClickListener {
 		TextView stayDuration = (TextView) v.findViewById(R.id.textView_stayDuration);
 		TextView departureVicinity = (TextView) v.findViewById(R.id.textView_departureLocation);
 		TextView departureTime = (TextView) v.findViewById(R.id.textView_departureTime);
-		
+
 		arrivalVicinity.setVisibility(View.GONE);
 		arrivalTime.setVisibility(View.GONE);
 		travelDistance.setVisibility(View.GONE);
@@ -116,25 +110,28 @@ public final class Itinerary extends Activity implements OnClickListener {
 		stayDuration.setVisibility(View.GONE);
 		departureVicinity.setVisibility(View.GONE);
 		departureTime.setVisibility(View.GONE);
-		
-		//TODO: define custom color scheme for this item
-		//destinationName.setTextColor
-		
-		itineraryItemList.add(andNewItemItem);
+
+		// TODO: define custom color scheme for this item
+		// destinationName.setTextColor(Color.BLACK);
+		// destinationName.setBackgroundColor(Color.WHITE);
+
+		itineraryItemList.add(addNewItemItem);
 		itineraryItems.add(itineraryItemList.get(itineraryItemList.size() - 1));
 	}
-	
+
 	private void configureItineraryListViewLongClickListener() {
 		itineraryListView.setOnItemLongClickListener(new OnItemLongClickListener() {
-			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {				
+			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+				if (position == (itineraryItemList.size() - 1)) {
+					StartSearchActivity();
+				}
 				return true;
 			}
 		});
 	}
 
 	private void waitForGps() {
-		waitSpinner = ProgressDialog.show(Itinerary.this, "",
-				"waiting for location...", true);
+		waitSpinner = ProgressDialog.show(Itinerary.this, "", "waiting for location...", true);
 		waitingForGps = true;
 		new Thread() {
 			@Override
@@ -205,14 +202,12 @@ public final class Itinerary extends Activity implements OnClickListener {
 	}
 
 	private void StartSearchActivity() {
-		Intent startItineraryOpen = new Intent(Itinerary.this,
-				PlacesSearchActivity.class);
+		Intent startItineraryOpen = new Intent(Itinerary.this, PlacesSearchActivity.class);
 
 		if (itineraryItemList.isEmpty()) {
 			startItineraryOpen.putExtra("location", currentLocation);
 		} else {
-			startItineraryOpen.putExtra("location", GetLastDestination()
-					.getLocation());
+			startItineraryOpen.putExtra("location", getLastDestination().getLocation());
 		}
 
 		startActivityForResult(startItineraryOpen, GET_NEW_DESTINATION);
@@ -220,7 +215,7 @@ public final class Itinerary extends Activity implements OnClickListener {
 
 	private void updateOrigin() {
 		origin.getSchedule().setDepartureTime(new Date());
-		if(currentLocation != null) {
+		if (currentLocation != null) {
 			origin.updateLocation(currentLocation);
 		}
 		itineraryItems.notifyDataSetChanged();
@@ -231,39 +226,42 @@ public final class Itinerary extends Activity implements OnClickListener {
 		Schedule departNow = new Schedule();
 		departNow.setDepartureTime(new Date());
 		origin.setSchedule(departNow);
-		itineraryItemList.add(origin);
-		itineraryItems.add(itineraryItemList.get(itineraryItemList.size() - 1));
+		if (currentLocation != null) {
+			origin.updateLocation(currentLocation);
+		}
+		itineraryItemList.add(0, origin);
+		itineraryItems.insert(itineraryItemList.get(0), 0);
 
 		new Thread() {
 			@Override
 			public void run() {
 				Calendar nextMinute = Calendar.getInstance();
-				nextMinute.setTime(new Date());				
+				nextMinute.setTime(new Date());
 				nextMinute.set(Calendar.MINUTE, nextMinute.get(Calendar.MINUTE) + 1);
 				nextMinute.set(Calendar.SECOND, 0);
-							
+
 				while (true) {
 					Calendar now = Calendar.getInstance();
 					now.setTime(new Date());
-					while (now.getTimeInMillis() < nextMinute.getTimeInMillis()) {						
+					while (now.getTimeInMillis() < nextMinute.getTimeInMillis()) {
 						try {
 							sleep(nextMinute.getTimeInMillis() - now.getTimeInMillis());
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
-						}					
+						}
 						now.setTime(new Date());
 					}
-					
+
 					class OriginUpdater implements Runnable {
 						public void run() {
 							origin.getSchedule().setDepartureTime(new Date());
-							itineraryItems.notifyDataSetChanged();							
+							itineraryItems.notifyDataSetChanged();
 						}
 					}
-					
+
 					Itinerary.this.runOnUiThread(new OriginUpdater());
-										
+
 					nextMinute.setTime(new Date());
 					nextMinute.set(Calendar.MINUTE, nextMinute.get(Calendar.MINUTE) + 1);
 				}
@@ -280,9 +278,8 @@ public final class Itinerary extends Activity implements OnClickListener {
 			// TODO: Wait for location service if current location is null
 
 			try {
-				ItineraryItem lastDestination = GetLastDestination();
-				myLocation = new ItineraryItem(currentLocation,
-						lastDestination.getLocation());
+				ItineraryItem lastDestination = getLastDestination();
+				myLocation = new ItineraryItem(currentLocation, lastDestination.getLocation());
 			} catch (IllegalStateException e) {
 				myLocation = new ItineraryItem(currentLocation);
 			}
@@ -291,13 +288,16 @@ public final class Itinerary extends Activity implements OnClickListener {
 		}
 	}
 
-	private ItineraryItem GetLastDestination() {
-		if (itineraryItemList.size() == 0) {
-			throw new IllegalStateException(
-					"The destination list is empty.  There is no previous destination");
+	private int getLastDestinationIndex() {
+		if (itineraryItemList.size() < 2) {
+			throw new IllegalStateException("The destination list has not been initialize correctly.");
 		}
 
-		return itineraryItemList.get(itineraryItemList.size() - 1);
+		return (itineraryItemList.size() - 1);
+	}
+
+	private ItineraryItem getLastDestination() {
+		return itineraryItemList.get(getLastDestinationIndex());
 	}
 
 	@Override
@@ -306,18 +306,15 @@ public final class Itinerary extends Activity implements OnClickListener {
 		switch (requestCode) {
 		case GET_NEW_DESTINATION:
 			if (resultCode == Activity.RESULT_OK) {
-				ItineraryItem newDestination = (ItineraryItem) data
-						.getParcelableExtra("itineraryItem");
+				ItineraryItem newDestination = (ItineraryItem) data.getParcelableExtra("itineraryItem");
 				UpdateArrivalDepartureTimes(newDestination);
 			}
 			break;
 		case UPDATE_DESTINATION_SCHEDULE:
 			if (resultCode == Activity.RESULT_OK) {
-				ItineraryItem scheduledDestination = (ItineraryItem) data
-						.getParcelableExtra("destination");
-				itineraryItemList.add(scheduledDestination);
-				itineraryItems.add(itineraryItemList.get(itineraryItemList
-						.size() - 1));
+				ItineraryItem scheduledDestination = (ItineraryItem) data.getParcelableExtra("destination");
+				itineraryItemList.add(getLastDestinationIndex(), scheduledDestination);
+				itineraryItems.insert(getLastDestination(), getLastDestinationIndex());
 			}
 			break;
 		default:
@@ -325,28 +322,19 @@ public final class Itinerary extends Activity implements OnClickListener {
 	}
 
 	private void UpdateArrivalDepartureTimes(ItineraryItem newDestination) {
-		Intent startDestinationSchedule = new Intent(Itinerary.this,
-				DestinationScheduleActivity.class);
+		Intent startDestinationSchedule = new Intent(Itinerary.this, DestinationScheduleActivity.class);
 		Calendar arrivalTimeCalculator = Calendar.getInstance();
 		if (!itineraryItemList.isEmpty()) {
-			ItineraryItem lastDestination = GetLastDestination();
-			arrivalTimeCalculator.setTime(lastDestination.getSchedule()
-					.getDepartureTime());
-			arrivalTimeCalculator.setTimeInMillis(arrivalTimeCalculator
-					.getTimeInMillis()
-					+ (newDestination.getTravelDuration() * 1000));
+			ItineraryItem lastDestination = itineraryItemList.get(itineraryItemList.size() - 2);
+			arrivalTimeCalculator.setTime(lastDestination.getSchedule().getDepartureTime());
+			arrivalTimeCalculator.setTimeInMillis(arrivalTimeCalculator.getTimeInMillis() + (newDestination.getTravelDuration() * 1000));
 		} else {
-			arrivalTimeCalculator.setTimeInMillis(arrivalTimeCalculator
-					.getTimeInMillis()
-					+ (newDestination.getTravelDuration() * 1000));
+			arrivalTimeCalculator.setTimeInMillis(arrivalTimeCalculator.getTimeInMillis() + (newDestination.getTravelDuration() * 1000));
 		}
-		newDestination.getSchedule().setArrivalTime(
-				arrivalTimeCalculator.getTime());
+		newDestination.getSchedule().setArrivalTime(arrivalTimeCalculator.getTime());
 
-		startDestinationSchedule.putExtra("vanleer.android.aeon.destination",
-				newDestination);
-		startActivityForResult(startDestinationSchedule,
-				UPDATE_DESTINATION_SCHEDULE);
+		startDestinationSchedule.putExtra("vanleer.android.aeon.destination", newDestination);
+		startActivityForResult(startDestinationSchedule, UPDATE_DESTINATION_SCHEDULE);
 	}
 
 	public void onClick(View v) {
