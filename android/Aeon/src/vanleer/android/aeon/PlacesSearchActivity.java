23999abf4b94cf09cc885c8f1dd973c0e12cd659
kvanleer <kevin.vanleer@gmail.com>
2014-01-05 10:14:33 -0600

    Removed Handler in favor of runInUiThread and Runnable

--- android/Aeon/src/vanleer/android/aeon/PlacesSearchActivity.java
+++ android/Aeon/src/vanleer/android/aeon/PlacesSearchActivity.java
@@ -12,8 +12,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -31,28 +29,28 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 	private ArrayList<ItineraryItem> searchResultsList;
 	private SearchResultItemAdapter searchResults;
 	private ListView searchResultsListView;
-	private int listViewId = R.id.listView_searchResults;
-	private String apiKey = "AIzaSyCXMEFDyFQK2Wu0-w0dyxs-nEO3uZoXUCc";
+	private final int listViewId = R.id.listView_searchResults;
+	private final String apiKey = "AIzaSyCXMEFDyFQK2Wu0-w0dyxs-nEO3uZoXUCc";
 	private Location currentLocation = null;
 	private ImageButton searchButton;
 	private TextView locationText;
 	private ImageView locationSensorImage;
 	private GooglePlacesSearch googleSearch;
 	private LocationManager locationManager;
-	private ProgressDialog waitSpinner = null; 
+	private ProgressDialog waitSpinner = null;
 	private AutoCompleteTextView searchText;
-	private boolean waitingForGps = false; 
+	private boolean waitingForGps = false;
 	private boolean searching = false;
-	private Long searchRadius = (long) 5000000;
+	private final Long searchRadius = (long) 5000000;
 	private UnfilteredArrayAdapter<String> suggestionList;
 
-	//4812 Danielle CT Granite City IL 62040 38.74419380,-90.09839319999999
-	//lat=38.74419380
-	//lng=-90.09839319999999
+	// 4812 Danielle CT Granite City IL 62040 38.74419380,-90.09839319999999
+	// lat=38.74419380
+	// lng=-90.09839319999999
 
-	//283 STONEHENGE DR WASHINGTON, MO 63090-4312
-	//38.477548,-91.051562
-	
+	// 283 STONEHENGE DR WASHINGTON, MO 63090-4312
+	// 38.477548,-91.051562
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -70,8 +68,7 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 		try {
 			waitSpinner.dismiss();
 			waitSpinner = null;
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 		}
 	}
 
@@ -83,36 +80,31 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 		locationText.setText("Waiting for location...");
 		searchButton = (ImageButton) findViewById(R.id.imageButton_search);
 		searchButton.setOnClickListener(this);
-		suggestionList = new UnfilteredArrayAdapter<String>(
-				this, android.R.layout.simple_dropdown_item_1line);
+		suggestionList = new UnfilteredArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line);
 		searchText = (AutoCompleteTextView) findViewById(R.id.editText_searchQuery);
 		searchText.setAdapter(suggestionList);
 		searchResultsList = new ArrayList<ItineraryItem>();
-		searchResults = new SearchResultItemAdapter(
-				this, R.layout.search_result_item, searchResultsList);
+		searchResults = new SearchResultItemAdapter(this, R.layout.search_result_item, searchResultsList);
 		searchResultsListView = (ListView) findViewById(listViewId);
 		searchResultsListView.setAdapter(searchResults);
 		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 	}
 
-
-	private void ConfigureLocationManager() {		
+	private void ConfigureLocationManager() {
 		LocationListener locationListener = CreateLocationListener();
 
-		if(getIntent() != null && getIntent().getExtras() != null) {
+		if (getIntent() != null && getIntent().getExtras() != null) {
 			currentLocation = getIntent().getExtras().getParcelable("location");
 		}
 
-		if(currentLocation == null) {
+		if (currentLocation == null) {
 			// Register the listener with the Location Manager to receive location updates
-			locationManager.requestLocationUpdates(
-					LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-			//TODO: locationManager.requestSingleUpdate(
-			//		LocationManager.GPS_PROVIDER, locationListener, some looper thing);
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+			// TODO: locationManager.requestSingleUpdate(
+			// LocationManager.GPS_PROVIDER, locationListener, some looper thing);
 		} else {
 			locationText.setText(GooglePlacesSearch.GetGeodeticString(currentLocation));
-			if(currentLocation.getProvider().equals(LocationManager.GPS_PROVIDER))
-			{
+			if (currentLocation.getProvider().equals(LocationManager.GPS_PROVIDER)) {
 				locationSensorImage.setVisibility(View.VISIBLE);
 			}
 			locationText.setText(googleSearch.getReverseGeocodeDescription(currentLocation, true));
@@ -124,9 +116,15 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 			public void onLocationChanged(Location location) {
 				makeUseOfNewLocation(location);
 			}
-			public void onStatusChanged(String provider, int status, Bundle extras) {}
-			public void onProviderEnabled(String provider) {}
-			public void onProviderDisabled(String provider) {}
+
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+			}
+
+			public void onProviderEnabled(String provider) {
+			}
+
+			public void onProviderDisabled(String provider) {
+			}
 		};
 	}
 
@@ -145,7 +143,7 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 	private void ConfigureTextWatcher() {
 		searchText.addTextChangedListener(new TextWatcher() {
 			public void onTextChanged(final CharSequence s, int start, int before, int count) {
-				if(s.length() > 1) {
+				if (s.length() > 1) {
 					new AsyncTask<CharSequence, Void, ArrayList<String>>() {
 						@Override
 						protected ArrayList<String> doInBackground(CharSequence... arg0) {
@@ -159,21 +157,24 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 					}.execute(s);
 				}
 			}
-			
-			public void afterTextChanged(Editable s) {}
-			public void beforeTextChanged(CharSequence s, int start, int count,	int after) {}
+
+			public void afterTextChanged(Editable s) {
+			}
+
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
 		});
 	}
 
 	private ArrayList<String> performAutocompleteSearch(CharSequence s) {
 		String input = s.toString();
 		ArrayList<String> results = null;
-		if(input != "") {
+		if (input != "") {
 			Double latitude = null;
 			Double longitude = null;
 			Double radius = null;
 
-			if(currentLocation != null) {
+			if (currentLocation != null) {
 				latitude = currentLocation.getLatitude();
 				longitude = currentLocation.getLongitude();
 				radius = (double) 1000;
@@ -188,8 +189,8 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 
 	private void updateAutocompleteChoices(ArrayList<String> suggestions) {
 		suggestionList.clear();
-		if(suggestions != null) {
-			for(String suggestion : suggestions) {
+		if (suggestions != null) {
+			for (String suggestion : suggestions) {
 				suggestionList.add(suggestion);
 			}
 		}
@@ -205,41 +206,45 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 			@Override
 			protected ArrayList<String> doInBackground(Void... arg0) {
 				ArrayList<String> suggestions = null;
-				Message msg = updateCurrentLocationTextHandler.obtainMessage();
-				msg.obj = googleSearch.getReverseGeocodeDescription(currentLocation, true);
-				updateCurrentLocationTextHandler.sendMessage(msg);
-				if(searchText.enoughToFilter()) {
+
+				class LocationTextUpdater implements Runnable {
+					private final String description;
+
+					LocationTextUpdater(String desc) {
+						description = desc;
+					}
+
+					public void run() {
+						locationText.setText(description);
+					}
+				}
+
+				PlacesSearchActivity.this.runOnUiThread(new LocationTextUpdater(googleSearch.getReverseGeocodeDescription(currentLocation, true)));
+
+				if (searchText.enoughToFilter()) {
 					suggestions = performAutocompleteSearch(searchText.getText());
 				}
 				return suggestions;
 			}
-			
+
 			@Override
 			protected void onPostExecute(ArrayList<String> suggestions) {
 				updateAutocompleteChoices(suggestions);
 			}
 		}.execute();
-		if(waitingForGps) {
-			waitingForGps  = false;
+		if (waitingForGps) {
+			waitingForGps = false;
 			onClick(searchButton);
 		}
 	}
 
-	final Handler updateCurrentLocationTextHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			locationText.setText((String) msg.obj);
-		}
-	};
-
 	private void WaitForGps() {
-		waitSpinner = ProgressDialog.show(PlacesSearchActivity.this,
-				"", "waiting for location...", true);
+		waitSpinner = ProgressDialog.show(PlacesSearchActivity.this, "", "waiting for location...", true);
 		waitingForGps = true;
 		new Thread() {
 			@Override
 			public void run() {
-				while(currentLocation == null) {
+				while (currentLocation == null) {
 					try {
 						sleep(1);
 					} catch (InterruptedException e) {
@@ -253,13 +258,12 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 	}
 
 	private void WaitForSearchResults() {
-		waitSpinner = ProgressDialog.show(PlacesSearchActivity.this,
-				"", "searching...", true);
+		waitSpinner = ProgressDialog.show(PlacesSearchActivity.this, "", "searching...", true);
 		searching = true;
 		new Thread() {
 			@Override
 			public void run() {
-				while(searching) {
+				while (searching) {
 					try {
 						sleep(1);
 					} catch (InterruptedException e) {
@@ -273,7 +277,7 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 	}
 
 	public void onClick(View v) {
-		switch(v.getId()) {
+		switch (v.getId()) {
 		case R.id.imageButton_search:
 			GetSearchResults();
 			break;
@@ -282,8 +286,8 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 		}
 	}
 
-	private void GetSearchResults() {	
-		if(currentLocation == null) {
+	private void GetSearchResults() {
+		if (currentLocation == null) {
 			WaitForGps();
 		} else {
 			WaitForSearchResults();
@@ -291,24 +295,23 @@ public final class PlacesSearchActivity extends Activity implements OnClickListe
 			searchResultsList.clear();
 			searchResults.clear();
 			searchResultsListView.clearChoices();
-			QuerySearchEngine();		
+			QuerySearchEngine();
 
 			BuildResultsList();
 		}
 	}
 
 	private void QuerySearchEngine() {
-		//searchRadius = (long) 5000000;
-		googleSearch.PerformSearch(currentLocation.getLatitude(), currentLocation.getLongitude(),
-				searchRadius, searchText.getText().toString(), true);
-		searching = false;		
+		// searchRadius = (long) 5000000;
+		googleSearch.PerformSearch(currentLocation.getLatitude(), currentLocation.getLongitude(), searchRadius, searchText.getText().toString(), true);
+		searching = false;
 	}
 
 	private void BuildResultsList() {
-		for(int i = 0; i < googleSearch.GetResultCount(); ++i) {
+		for (int i = 0; i < googleSearch.GetResultCount(); ++i) {
 			ItineraryItem newItem = googleSearch.GetPlace(i);
-			if(newItem != null) {
-				//newItem.SetDistance(currentLocation);
+			if (newItem != null) {
+				// newItem.SetDistance(currentLocation);
 				searchResultsList.add(newItem);
 				searchResults.add(searchResultsList.get(i));
 			}
