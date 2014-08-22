f2ebae733b93c4011f202fc11bca2da49044c8a4
kvanleer <kevin.vanleer@gmail.com>
2014-08-21 21:50:53 -0500

    Removed unused imports

--- android/Aeon/src/vanleer/android/aeon/DepartureAlarm.java
+++ android/Aeon/src/vanleer/android/aeon/DepartureAlarm.java
@@ -13,7 +13,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AnalogClock;
 import android.widget.Button;
 import android.widget.TextView;
 
