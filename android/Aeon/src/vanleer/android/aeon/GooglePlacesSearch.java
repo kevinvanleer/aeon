118434a05862e62ce44932e9b717b26949a067a5
kvanleer <kevin.vanleer@gmail.com>
2014-08-23 08:05:15 -0500

    Added interface to query directions api and class to hold result

--- android/Aeon/src/vanleer/android/aeon/GooglePlacesSearch.java
+++ android/Aeon/src/vanleer/android/aeon/GooglePlacesSearch.java
@@ -31,6 +31,82 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
+class DirectionsResult {
+	private JSONObject rawJson = null;
+
+	DirectionsResult(JSONObject rawJson) {
+		this.rawJson = rawJson;
+	}
+
+	public int getRouteCount() {
+
+		int routeCount = 0;
+
+		JSONArray routeArray = getRoutes();
+		if (routeArray != null) {
+			routeCount = routeArray.size();
+		}
+
+		return routeCount;
+	}
+
+	private JSONArray getRoutes() {
+		if (rawJson == null) {
+			throw new NullPointerException();
+		}
+
+		return (JSONArray) rawJson.get("routes");
+	}
+
+	public String getSummary() {
+		return getSummary((JSONObject) getRoutes().get(0));
+	}
+
+	public String getSummary(JSONObject route) {
+		return (String) route.get("summary");
+	}
+
+	private JSONArray getLegs(JSONObject route) {
+		return (JSONArray) route.get("legs");
+	}
+
+	private JSONArray getSteps(JSONObject leg) {
+		return (JSONArray) leg.get("steps");
+	}
+
+	public Location getOrigin() {
+		return getOrigin();
+	}
+
+	public Address getOrigin(JSONObject route) {
+		Address originAddress = new Address(null);
+
+		JSONArray legs = getLegs(route);
+		JSONObject firstLeg = (JSONObject) legs.get(0);
+		JSONObject startLocation = (JSONObject) firstLeg.get("start_location");
+
+		originAddress.setLatitude((Double) startLocation.get("lat"));
+		originAddress.setLongitude((Double) startLocation.get("lng"));
+		originAddress.setAddressLine(0, (String) startLocation.get("start_address"));
+
+		return originAddress;
+	}
+
+	public Address getDestination(JSONObject route) {
+		Address destinationAddress = new Address(null);
+
+		JSONArray legs = getLegs(route);
+		JSONObject firstLeg = (JSONObject) legs.get(legs.size() - 1);
+		JSONObject endLocation = (JSONObject) firstLeg.get("end_location");
+
+		destinationAddress.setLatitude((Double) endLocation.get("lat"));
+		destinationAddress.setLongitude((Double) endLocation.get("lng"));
+		destinationAddress.setAddressLine(0, (String) endLocation.get("end_address"));
+
+		return destinationAddress;
+	}
+}
+
 public final class GooglePlacesSearch {
 	private static final String GOOGLE_PLACES_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/search/json";
 	private static final String GOOGLE_PLACES_AUTOCOMPLETE_URL = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
@@ -220,6 +296,25 @@ public final class GooglePlacesSearch {
 		url += "&key=" + apiKey;
 
 		return url;
+	}
+
+	public DirectionsResult performDirectionsQuery(Location origin, Location destination) {
+		String url = buildGoogleDirectionsUrl(origin, destination);
+		return new DirectionsResult(performHttpGet(url));
+	}
+
+	public ArrayList<String> performDirectionsQuery(Location origin, Location destination, ArrayList<Location> waypoints) {
+		String url = buildGoogleDirectionsUrl(origin, destination, waypoints);
+
+		JSONObject directionsResults = performHttpGet(url);
+		return getResultsList(directionsResults);
+	}
+
+	public ArrayList<String> performDirectionsQuery(Location origin, Location destination, ArrayList<Location> waypoints, String mode) {
+		String url = buildGoogleDirectionsUrl(origin, destination, waypoints, mode);
+
+		JSONObject directionsResults = performHttpGet(url);
+		return getResultsList(directionsResults);
 	}
 
 	private String getTypesUrlPart(String[] types) {
